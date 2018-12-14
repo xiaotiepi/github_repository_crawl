f7d55efe0eaf19f77e00de3632383a4862ecb0a9
xiaotiepi <45758970+xiaotiepi@users.noreply.github.com>
2018-12-14 21:06:32 +0800

    add new item

--- shiyanlou/shiyanlou/spiders/githubspider.py
+++ shiyanlou/shiyanlou/spiders/githubspider.py
@@ -12,11 +12,20 @@ class GithubspiderSpider(scrapy.Spider):
     'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yMVQxODowOTowMiswODowMM4BnQBZ&tab=repositories'
     ]
 
+
     def parse(self, response):
         for repository in response.css('li.public'):
-            yield RepositoryItem({
-            	# 'name':repository.xpath('.//a[@itemprop="name codeRepository"]/text()').extract_first().strip(),
-                'name':repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first("\n\s*(.*)"),
-                'update_time':repository.xpath('.//relative-time/@datetime').extract_first()
-                })
+            item = RepositoryItem()
+            item['name'] = repository.xpath('.//a[@itemprop="name codeRepository"]/text()').re_first("\n\s*(.*)")
+            item['update_time'] = repository.xpath('.//relative-time/@datetime').extract_first()
+            project_url = response.urljoin(repository.xpath('.//a[@itemprop="name codeRepository"]/@href').extract_first())
+            yield scrapy.Request(project_url,callback=self.parse_next,meta=item)
+            
+    def parse_next(self,response):
+        item = response.meta
+        for num in response.xpath('//li[@class="commits"]'):
+            item['commits'] = num.xpath('//span[@class="num text-emphasized"]/text()').re('\n\s*(.*)\n')[0]
+            item['branchs'] = num.xpath('//span[@class="num text-emphasized"]/text()').re('\n\s*(.*)\n')[1]
+            item['releases'] = num.xpath('//span[@class="num text-emphasized"]/text()').re('\n\s*(.*)\n')[2]
+        yield item
  
